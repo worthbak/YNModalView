544bcab78805d90492e7728693e45449c5eacb91
Worth Baker <wbaker@greenmtd.com>
2016-01-15 16:21:48 -0700

    added a garbage test as a, uh, test

---
+++ YNModalView/YNModalView.swift
@@ -0,0 +1,13 @@
+//
+//  YNModalView.swift
+//  YNModalView
+//
+//  Created by David Baker on 1/15/16.
+//  Copyright © 2016 Worth Baker. All rights reserved.
+//
+
+import Foundation
+
+public func YNModalViewRun(number: Int) -> Int {
+  return number
+}
--- YNModalViewTests/YNModalViewTests.swift
+++ YNModalViewTests/YNModalViewTests.swift
@@ -7,30 +7,37 @@
 //
 
 import XCTest
-@testable import YNModalView
+//@testable import YNModalView
+import YNModalView
 
 class YNModalViewTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(true)
+  }
+  
+  func testColorReturn() {
+    let fifteen = YNModalViewRun(15)
+    XCTAssertEqual(15, fifteen)
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measureBlock {
+      // Put the code you want to measure the time of here.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
