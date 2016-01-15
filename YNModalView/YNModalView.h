eab370471fcd3722b814ad93bef7b7943b07925c
Worth Baker <wbaker@greenmtd.com>
2016-01-15 15:31:18 -0700

    added Xcode project/workspace

---
+++ YNModalView/YNModalView.h
@@ -0,0 +1,19 @@
+//
+//  YNModalView.h
+//  YNModalView
+//
+//  Created by David Baker on 1/15/16.
+//  Copyright © 2016 Worth Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YNModalView.
+FOUNDATION_EXPORT double YNModalViewVersionNumber;
+
+//! Project version string for YNModalView.
+FOUNDATION_EXPORT const unsigned char YNModalViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YNModalView/PublicHeader.h>
+
+
